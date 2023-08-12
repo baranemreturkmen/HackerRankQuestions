40e64d7699e6387363ae2369b5aeebeb2af0416f
Baran Emre Turkmen <baranemre1997@gmail.com>
2023-08-12 11:34:24 +0300

    12.08.2023 SubstringRemoval TODO solved.

--- HackerRank-Questions/src/main/java/SubstringRemoval.java
+++ HackerRank-Questions/src/main/java/SubstringRemoval.java
@@ -4,13 +4,33 @@ import java.util.List;
 public class SubstringRemoval {
 
     static List<String> stringList = new ArrayList<>();
+    private static final long NANO_TO_MILLISECONDS_CONSTANT = 1_000_000;
 
     public static void main(String[] args) {
 
+        long startTime;
+        long endTime;
+
         initializeStrings();
+        startTime = System.nanoTime();
         for (String str: stringList){
             System.out.println(getMinLength(str));
         }
+        endTime = System.nanoTime();
+        System.out.println("Calculated approximate time for first approach is: "+(endTime-startTime)+" nano seconds");
+        //132083 nano seconds
+
+        System.out.println("--------------------------");
+
+        startTime = System.nanoTime();
+        for (String str: stringList){
+            System.out.println(getMinLengthWithOnlyOneLoop(str));
+        }
+        endTime = System.nanoTime();
+        System.out.println("Calculated approximate time for second approach is: "+(endTime-startTime)+" nano seconds");
+        //40667 nano seconds
+
+        //Second approach is approximately 4 times faster than firs approach
     }
 
     private static List<String> initializeStrings(){
@@ -21,7 +41,6 @@ public class SubstringRemoval {
         return stringList;
     }
 
-    //TODO: Change the getMinLength function only one loop version.
     private static int getMinLength(String seq){
         while(seq.indexOf("AB") != -1){
             seq = seq.replace("AB","");
@@ -29,6 +48,19 @@ public class SubstringRemoval {
 
         while(seq.indexOf("BB") != -1){
             seq = seq.replace("BB","");
+        }
+
+        return seq.length();
+    }
+
+    private static int getMinLengthWithOnlyOneLoop(String seq){
+        while(seq.indexOf("AB") != -1 || seq.indexOf("BB") != -1){
+            if(seq.indexOf("AB") != -1){
+                seq = seq.replace("AB","");
+            }
+            else if(seq.indexOf("BB") != -1){
+                seq = seq.replace("BB","");
+            }
         }
 
         return seq.length();
