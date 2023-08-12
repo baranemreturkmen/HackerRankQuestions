56fd761485933f4f959b2887d8aecf3b03d901a8
Baran Emre Turkmen <baranemre1997@gmail.com>
2023-08-12 16:24:49 +0300

    12.08.2023 Anagrams solution improved by only one loop.

--- HackerRank-Questions/src/main/java/Anagrams.java
+++ HackerRank-Questions/src/main/java/Anagrams.java
@@ -5,15 +5,31 @@ import java.util.stream.Collectors;
 public class Anagrams {
 
     static List<String> anagramList = new ArrayList<>();
+    private static final long NANO_TO_MILLISECONDS_CONSTANT = 1_000_000;
 
     public static void main(String[] args) {
 
-        initializeAnagramLists();
+        long startTime;
+        long endTime;
 
+        initializeAnagramLists();
+        startTime = System.nanoTime();
         for(String str: anagramList){
             System.out.println(findMinimumAttemptForAnagram(str));
-            System.out.println("---------------------");
         }
+        endTime = System.nanoTime();
+        System.out.println("Calculated approximate time for first approach is: "+(endTime-startTime)/NANO_TO_MILLISECONDS_CONSTANT+" milliseconds");
+        //Too close to 2 milliseconds
+
+        System.out.println("--------------------------------");
+
+        startTime = System.nanoTime();
+        for(String str: anagramList){
+            System.out.println(findMinimumAttemptForAnagramWithOnlyOneLoop(str));
+        }
+        endTime = System.nanoTime();
+        System.out.println("Calculated approximate time for first approach is: "+(endTime-startTime)/NANO_TO_MILLISECONDS_CONSTANT+" milliseconds");
+        //Too close to 0.
 
     }
 
@@ -52,6 +68,33 @@ public class Anagrams {
                 else if(j==charListForSecondHalf.size()-1){
                     attemptNumber+=1;
                 }
+            }
+        }
+
+        return attemptNumber;
+    }
+
+    private static int findMinimumAttemptForAnagramWithOnlyOneLoop(String str){
+
+        int attemptNumber = 0;
+
+        String strFirstHalf = str.substring(0,str.length()/2);
+        String strSecondHalf = str.substring(str.length()/2);
+
+        List<Character> charListForFirstHalf = strFirstHalf.chars()
+                .mapToObj(e->(char)e).sorted().collect(Collectors.toList());
+
+        List<Character> charListForSecondHalf = strSecondHalf.chars()
+                .mapToObj(e->(char)e).sorted().collect(Collectors.toList());
+
+        if(charListForFirstHalf.equals(charListForSecondHalf)){
+            System.out.println("No need to change!");
+            return attemptNumber;
+        }
+
+        for(int i=0;i<charListForFirstHalf.size();i++){
+            if(!charListForSecondHalf.contains(charListForFirstHalf.get(i))){
+                attemptNumber+=1;
             }
         }
 
