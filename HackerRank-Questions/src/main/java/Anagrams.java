58e0e5502ab4191bf3152032e19b785c9fd8eeee
Baran Emre Turkmen <baranemre1997@gmail.com>
2023-08-13 20:36:39 +0300

    13.08.2023 Anagrams added another solution without lists and stream api. Also made some performance calculations.

--- HackerRank-Questions/src/main/java/Anagrams.java
+++ HackerRank-Questions/src/main/java/Anagrams.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 public class Anagrams {
 
     static List<String> anagramList = new ArrayList<>();
-    private static final long NANO_TO_MILLISECONDS_CONSTANT = 1_000_000;
 
     public static void main(String[] args) {
 
@@ -18,8 +17,8 @@ public class Anagrams {
             System.out.println(findMinimumAttemptForAnagram(str));
         }
         endTime = System.nanoTime();
-        System.out.println("Calculated approximate time for first approach is: "+(endTime-startTime)/NANO_TO_MILLISECONDS_CONSTANT+" milliseconds");
-        //Too close to 2 milliseconds
+        System.out.println("Calculated approximate time for first approach is: "+(endTime-startTime)+" nano seconds");
+        //2139500 nano seconds
 
         System.out.println("--------------------------------");
 
@@ -28,8 +27,22 @@ public class Anagrams {
             System.out.println(findMinimumAttemptForAnagramWithOnlyOneLoop(str));
         }
         endTime = System.nanoTime();
-        System.out.println("Calculated approximate time for first approach is: "+(endTime-startTime)/NANO_TO_MILLISECONDS_CONSTANT+" milliseconds");
-        //Too close to 0.
+        System.out.println("Calculated approximate time for second approach is: "+(endTime-startTime)+" nano seconds");
+        //473709 nano seconds
+
+        System.out.println("--------------------------------");
+
+        startTime = System.nanoTime();
+        for(String str: anagramList){
+            System.out.println(findMinimumAttemptForAnagramWithOnlyOneLoopWithoutLists(str));
+        }
+        endTime = System.nanoTime();
+        System.out.println("Calculated approximate time for third approach is: "+(endTime-startTime)+" nano seconds");
+        //385417 nano seconds
+        //Without if control 36917 nano seconds -2
+        /*I know sort is not important when stream converting to set. But I try for calculate the performance
+        * and I saw sort in stream effects time to 1189375 nano seconds.So we can say that sort in stream is
+        * performance killer! -3*/
 
     }
 
@@ -94,6 +107,36 @@ public class Anagrams {
 
         for(int i=0;i<charListForFirstHalf.size();i++){
             if(!charListForSecondHalf.contains(charListForFirstHalf.get(i))){
+                attemptNumber+=1;
+            }
+        }
+
+        return attemptNumber;
+    }
+
+    private static int findMinimumAttemptForAnagramWithOnlyOneLoopWithoutLists(String str){
+
+        int attemptNumber = 0;
+
+        String strFirstHalf = str.substring(0,str.length()/2);
+        String strSecondHalf = str.substring(str.length()/2);
+
+        /*if(strFirstHalf.chars().mapToObj(v-> (char) v).collect(Collectors.toSet())
+                .equals(strSecondHalf.chars().mapToObj(v-> (char) v).collect(Collectors.toSet()))){
+            System.out.println("No need to change!");
+            return attemptNumber;
+        }*/
+        //2
+
+        /*if(strFirstHalf.chars().mapToObj(v-> (char) v).sorted().collect(Collectors.toSet())
+                .equals(strSecondHalf.chars().mapToObj(v-> (char) v).sorted().collect(Collectors.toSet()))){
+            System.out.println("No need to change!");
+            return attemptNumber;
+        }*/
+        //3
+
+        for(int i=0;i<strFirstHalf.length();i++){
+            if(!strSecondHalf.contains(strFirstHalf.substring(i,i+1))){
                 attemptNumber+=1;
             }
         }
